5032414f6359dfa726f4005826c4f9a3e33c871d
Mark Conroy <mark@mark.ie>
2022-08-12 13:52:08 +0200

    sets facet 1 to always be open

--- web/themes/custom/anrt_lgd/js/facets.js
+++ web/themes/custom/anrt_lgd/js/facets.js
@@ -12,46 +12,54 @@ if (window.NodeList && !NodeList.prototype.forEach) {
   Drupal.behaviors.lgdFacets = {
     attach: function (context) {
       context = context || document;
-      
-      const facets = context.querySelectorAll('.facets-widget__list');
-      const selectedFacets = context.querySelectorAll('.facets-widget input[checked]');
 
-      facets.forEach(facet => {
-        if (!facet.classList.contains('js-processed')) {
-          facet.classList.add('js-processed');
+      const facets = context.querySelectorAll(".facets-widget__list");
+      const selectedFacets = context.querySelectorAll(
+        ".facets-widget input[checked]"
+      );
+
+      facets.forEach((facet, index) => {
+        if (!facet.classList.contains("js-processed")) {
+          facet.classList.add("js-processed");
           const expand = facet.previousElementSibling;
-          const trigger = expand.querySelector('.facet-group__trigger');
-          const facetItems = facet.querySelectorAll('.facet-item');
+          const trigger = expand.querySelector(".facet-group__trigger");
+          const facetItems = facet.querySelectorAll(".facet-item");
 
-          // If there's 3 or fewer facets available, show all the facets in that block.
-          if (facetItems.length <= 3) {
-            facet.style.display = 'block';
-            trigger.setAttribute('aria-expanded', 'true');
+          // Always keep the first facet expanded
+          if (index === 0) {
+            facet.style.display = "block";
+            trigger.setAttribute("aria-expanded", "true");
           }
-
-          expand.addEventListener('click', function() {
-            const expanded = trigger.getAttribute('aria-expanded');
-            if (expanded === 'false') {
-              facet.style.display = 'block';
-              trigger.setAttribute('aria-expanded', 'true');
+          // If there's 3 or fewer facets available, show all the facets in that block.
+          else if (facetItems.length <= 3) {
+            facet.style.display = "block";
+            trigger.setAttribute("aria-expanded", "true");
+          }
+          
+          expand.addEventListener("click", function () {
+            const expanded = trigger.getAttribute("aria-expanded");
+            if (expanded === "false") {
+              facet.style.display = "block";
+              trigger.setAttribute("aria-expanded", "true");
             } else {
-              facet.style.display = 'none';
-              trigger.setAttribute('aria-expanded', 'false');
+              facet.style.display = "none";
+              trigger.setAttribute("aria-expanded", "false");
             }
           });
         }
       });
 
-      selectedFacets.forEach(selectedFacet => {
-        if (!selectedFacet.classList.contains('js-processed')) {
-          selectedFacet.classList.add('js-processed');
-          const facetList = selectedFacet.closest('.facets-widget__list');
-          const trigger = facetList.previousElementSibling.querySelector('.facet-group__trigger');
-          trigger.setAttribute('aria-expanded', 'true');
-          facetList.style.display = 'block';
+      selectedFacets.forEach((selectedFacet) => {
+        if (!selectedFacet.classList.contains("js-processed")) {
+          selectedFacet.classList.add("js-processed");
+          const facetList = selectedFacet.closest(".facets-widget__list");
+          const trigger = facetList.previousElementSibling.querySelector(
+            ".facet-group__trigger"
+          );
+          trigger.setAttribute("aria-expanded", "true");
+          facetList.style.display = "block";
         }
       });
-  
-    }
+    },
   };
-}(Drupal));
+})(Drupal);
