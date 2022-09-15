996d9bb05a9f18d0f12b620ba06c847237052734
Alan Burke <alan@cycletraffic.com>
2022-09-15 15:41:02 +0200

    Add custom module to allow Directories to be checked by end users

---
+++ web/modules/custom/send_directory_check/src/Controller/CheckController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\send_directory_check\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckController extends ControllerBase {
+
+  /**
+   * Update the "Provider Last Checked" value on the node.
+   */
+  public function approve(NodeInterface $node) {
+    $node->set('field_provider_last_checked', date('Y-m-d\\TH:i:s'));
+    $node->save();
+    \Drupal::messenger()->addMessage(t('Thank you for confirming this content.'), 'status');
+    return $this->pageRedirect($node);
+  }
+
+  protected function pageRedirect(NodeInterface $node): Response {
+    return new RedirectResponse(
+      Url::fromRoute(
+        'entity.node.canonical',
+        [
+          'node' => $node->id(),
+        ],
+      )->toString(),
+    );
+  }
+
+}
